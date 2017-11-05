6cfa65e4babd7296c6758eb4065822fcea7e7946
Tiankai Xie <tiankaixie@gmail.com>
2017-11-05 01:18:06 -0700

    Finished 129

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from first_app.models import Topic, AccessRecord, WebPage
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(AccessRecord)
+admin.site.register(WebPage)
